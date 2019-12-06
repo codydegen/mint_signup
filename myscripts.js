3bbdd99f1d516ccc4856550508f62af2feca8166
codydegen <codydegen@gmail.com>
2019-12-06 17:49:46 -0500

    Add CSS to header

---
+++ myscripts.js
@@ -0,0 +1,14 @@
+const mintClick = document.getElementById('mint-icon');
+mintClick.addEventListener('click', () => {
+  window.open('https://www.mint.com');
+});
+
+const quickbooksClick = document.getElementById('quickbooks-icon');
+quickbooksClick.addEventListener('click', () => {
+  window.open('https://www.quickbooks.intuit.com');
+});
+
+const turbotaxClick = document.getElementById('turbotax-icon');
+turbotaxClick.addEventListener('click', () => {
+  window.open('https://www.turbotax.intuit.com');
+});
